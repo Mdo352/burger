74aad23c1b353bc7790f18adbde4bfa1d6b47e87
Mike <mjeanjacques90@gmail.com>
2019-07-21 21:23:44 -0400

    setup jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,17 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  // host: "localhost",
+  // port: 3306,
+  // user: "root",
+  // // password: SQL_PASSWORD,
+  // password: "HmYz#4g7p1pH",
+  // database: "burgers_db"
+  host: "tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  // password: SQL_PASSWORD,
-  password: "HmYz#4g7p1pH",
-  database: "burgers_db"
+  user: "ats8xzf6452y8tap	",
+  password: "ew1k9ao9f9xpu594",
+  database: "hk3p5ugfxouy9v75"
 });
 
 // Make connection.
